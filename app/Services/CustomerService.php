e4e16e501e05c18b57805d422c7f84f51b795c96
thuattranLock <thuat.how.to.code@gmail.com>
2021-01-03 21:12:12 +0700

    init

---
+++ app/Services/CustomerService.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Services;
+
+interface CustomerService
+{
+    public function getAll();
+    public function findById($id);
+    public function create($request);
+    public function update($request, $id);
+    public function destroy($id);
+}
