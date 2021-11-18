e26c38190d3a7437dae5f183a0c9c8111d3268ad
tranduythuat <thuattran1810@gmail.com>
2021-11-18 12:44:19 +0700

    two commit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//test route
+//test route fdsajfda
 Route::get('/', function () {
     return view('welcome');
 });
